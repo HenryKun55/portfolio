f82e41da25cddef2e14db17f471a4230df4922b2
Flávio Henrique <henriqueflavio55@gmail.com>
2020-08-16 05:01:03 -0300

    add projects in page Me

--- js/script.js
+++ js/script.js
@@ -13,3 +13,17 @@ function openNav() {
 function closeNav() {
   handleNav(115)
 }
+
+function tab(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "flex";
+  evt.currentTarget.className += " active";
+}
